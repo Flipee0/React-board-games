5ad7562b7ec4024913d8e4e525a9e60199ca966b
Flipee0 <dokweb0987@gmail.com>
2024-03-20 05:18:42 +0300

    Fix routing

--- src/app/providers/withRouter.tsx
+++ src/app/providers/withRouter.tsx
@@ -1,12 +1,12 @@
 import React, { Suspense } from "react";
-import { BrowserRouter } from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 const WithRouter = ({ children }: { children: React.ReactNode }) => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       {/*TODO: add fallback*/}
       <Suspense>{children}</Suspense>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
